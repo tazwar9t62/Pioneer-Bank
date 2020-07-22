cfd7ec90525eae413ab6d826381c8dba7aa2db03
Tazwar <tazwar9t6@gmail.com>
2020-07-22 15:57:35 +0600

    70% done

--- javaScript.js
+++ javaScript.js
@@ -3,4 +3,15 @@ let loginButton = document.getElementById("login-btn").addEventListener("click",
     login.style.display = ("none");
     let trasaction = document.getElementById("transaction-area");
     transaction.style.display = "block";
+    let depositBtn = document.getElementById("depositAmount").addEventListener("click", function () {
+        let depositInputValue = document.getElementById("depositValue").value;
+        let depositNumber = parseFloat(depositInputValue);
+
+        let currentDeposit = document.getElementById("currentDepost").innerText;
+        let depositNumber = parseFloat(currentdeposit);
+        let depositTotal = currentDeposit + depositNumber;
+        document.getElementById("currentDepost").innerText = depositTotal;
+    });
+
+
 })
