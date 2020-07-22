9ed88f455462218b3e71d6c2daad7ce8a3eae29e
Tazwar <tazwar9t6@gmail.com>
2020-07-22 13:31:32 +0600

    30% done

---
+++ javaScript.js
@@ -0,0 +1,6 @@
+let loginButton = document.getElementById("login-btn").addEventListener("click", function () {
+    let login = document.getElementById("login-area");
+    login.style.display = ("none");
+    let trasaction = document.getElementById("transaction-area");
+    transaction.style.display = "block";
+})
